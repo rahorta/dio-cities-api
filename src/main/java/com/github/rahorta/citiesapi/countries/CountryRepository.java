939c163ddd67c46494e95997e7bf1ca64c489dc1
Andrade Goulart Horta <ront@gft.com>
2021-12-29 22:55:07 -0300

    primeiro commit projeto API cities-api

---
+++ src/main/java/com/github/rahorta/citiesapi/countries/CountryRepository.java
@@ -0,0 +1,6 @@
+package com.github.rahorta.citiesapi.countries;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CountryRepository extends JpaRepository<Country, Long> {
+}
---
+++ src/main/java/com/github/rahorta/citiesapi/states/StateRepository.java
@@ -0,0 +1,6 @@
+package com.github.rahorta.citiesapi.states;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StateRepository extends JpaRepository<State, Long> {
+}
